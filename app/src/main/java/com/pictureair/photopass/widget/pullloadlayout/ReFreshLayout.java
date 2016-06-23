d2cae9b9b86444c4c1af40b236ca3c855dabfcf6
Peng <peng.wu@pictureworks.biz>
2016-06-23 19:03:57 +0800

    1、完善上拉加载功能 2、晚上PP选择页面 3、完善动画效果 4、修改提示信息

--- app/src/main/java/com/pictureair/photopass/widget/pullloadlayout/ReFreshLayout.java
+++ app/src/main/java/com/pictureair/photopass/widget/pullloadlayout/ReFreshLayout.java
@@ -1,9 +1,7 @@
 package com.pictureair.photopass.widget.pullloadlayout;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,15 +11,16 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.Scroller;
 import android.widget.TextView;
 
-import com.nineoldandroids.view.ViewHelper;
 import com.pictureair.photopass.R;
+import com.pictureair.photopass.util.PictureAirLog;
 
 /**
  * Created by pengwu on 16/6/16.
  */
-public class ReFreshLayout extends RelativeLayout {
+public class ReFreshLayout extends RelativeLayout{
 
 
     private float mStartY;
@@ -37,6 +36,7 @@ public class ReFreshLayout extends RelativeLayout {
     private int touchSlop;
     private MyListView listView;
     private int lastStatus = mState;
+    Scroller scroller;
     /**
      * 下拉状态
      */
@@ -60,6 +60,8 @@ public class ReFreshLayout extends RelativeLayout {
      * 当前是否可以下拉，只有ListView滚动到头的时候才允许下拉
      */
     private boolean ableToPull;
+    private int allMove;
+    private boolean isPull;
 
     public ReFreshLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -70,9 +72,9 @@ public class ReFreshLayout extends RelativeLayout {
         touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
         addView(header,0);
         mIsLoading = false;
-        header.setVisibility(GONE);
     }
     private boolean loadOnce;
+    //    LayoutParams lp;
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
@@ -88,30 +90,12 @@ public class ReFreshLayout extends RelativeLayout {
 
     }
 
-    public void setFootViewVisibility(int visibility) {
-        header.setVisibility(visibility);
-    }
-
     public void setOnRefreshListener(OnRefreshListener listener) {
         this.listener = listener;
     }
 
-    public void removeRefreshListener() {
-        this.listener = null;
-    }
-
-    public OnRefreshListener getOnRefreshListener(){
-        return this.listener;
-    }
-
     public void setListView (MyListView lv) {
         this.listView = lv;
-    }
-
-    private void translationY(View child,float distance){
-        if (child != null) {
-            ViewHelper.setTranslationY(child, distance);
-        }
     }
 
     @Override
@@ -120,24 +104,72 @@ public class ReFreshLayout extends RelativeLayout {
             return  super.dispatchTouchEvent(ev);
         }
         boolean handle = super.dispatchTouchEvent(ev);
-        handle |= onTouchEvent(ev);
+        if (ev.getAction() == MotionEvent.ACTION_DOWN || ev.getAction() == MotionEvent.ACTION_UP) {
+            if (ev.getAction() == MotionEvent.ACTION_UP) {
+                PictureAirLog.e("dispatchTouchEvent","ACTION_UP");
+            }
+            handle |= onTouchEvent(ev);
+        }
         return handle;
     }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (listView == null) {
+            return super.onInterceptTouchEvent(ev);
+        } else {
+            setIsAbleToPull(ev);
+            PictureAirLog.e("RefreshLayout", "move");
+            PictureAirLog.e("ableToPull", String.valueOf(ableToPull));
+            PictureAirLog.e("mIsLoading", String.valueOf(mIsLoading));
+            if (ableToPull && !mIsLoading && listView != null) {
+                if (isPull) {
+                    return true;
+                }
+                if (ev.getAction() == MotionEvent.ACTION_MOVE) {
+                    float tmpY = ev.getRawY();
+                    float space = Math.round(lastY - tmpY + 0.5f);
+                    if (lastY != tmpY) {
+                        lastY = tmpY;
+                    }
+                    PictureAirLog.e("RefreshLayout", "space");
+                    PictureAirLog.e("space", String.valueOf(space));
+                    PictureAirLog.e("allmove", String.valueOf(allMove));
+                    if (space > 5 && allMove >= 0) {
+                        isPull = true;
+                        return true;
+                    }
+                }
+            } else if (mIsLoading) {
+                if (ev.getAction() == MotionEvent.ACTION_MOVE) {
+                    return true;
+                }
+            }
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    float lastY;
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
+        if (listView == null) {
+            return super.onTouchEvent(ev);
+        }
         setIsAbleToPull(ev);
         if (ableToPull && !mIsLoading && listView != null) {
             switch (ev.getAction()) {
                 case MotionEvent.ACTION_DOWN:
                     mStartY = ev.getRawY();
+                    lastY = mStartY;
                     return true;
                 case MotionEvent.ACTION_MOVE:
                     float tmpY = ev.getRawY();
-                    float space = mStartY - tmpY;
-                    if (space <= 0 && headerLayoutParams.bottomMargin <= hideHeaderHeight) {
-                        return false;
+                    float mSpace = lastY - tmpY;
+                    int res = Math.round(mSpace/2+0.5f);
+                    if (lastY != tmpY) {
+                        lastY = tmpY;
                     }
-                    if (space < touchSlop) {
+                    if (headerLayoutParams.bottomMargin < hideHeaderHeight) {
                         return false;
                     }
                     if (mState != STATUS_REFRESHING) {
@@ -147,26 +179,33 @@ public class ReFreshLayout extends RelativeLayout {
                             mState = STATUS_PULL_TO_REFRESH;
                         }
                     }
-                    if (!listView.getIsParentMoving()) {
-                        listView.setIsParentMoving(true);
-                    }
-                    if (headerLayoutParams.bottomMargin < 10) {
-                        translationY(listView, -space / 2);
-                        headerLayoutParams.bottomMargin = (int) ((space / 2) + hideHeaderHeight);
-                        header.setLayoutParams(headerLayoutParams);
-                    }
+                    allMove += res;
+                    PictureAirLog.e("ontouchevent allMove",String.valueOf(allMove));
+                    listView.listScrollBy(0, res);
+                    headerLayoutParams.bottomMargin = res + headerLayoutParams.bottomMargin;
+                    header.setLayoutParams(headerLayoutParams);
                     break;
                 case MotionEvent.ACTION_CANCEL:
                 case MotionEvent.ACTION_UP:
-                    if (mState == STATUS_RELEASE_TO_REFRESH) {
+                    PictureAirLog.e("ontouchevent","ACTION_UP");
+                    if (mState == STATUS_RELEASE_TO_REFRESH && !mIsLoading) {
                         // 松手时如果是释放立即刷新状态，就去调用正在刷新的任务
-                        new RefreshingTask().execute();
-                        Log.e("Ontouc","STATUS_RELEASE_TO_REFRESH");
+                        listView.listScrollBy(0,-headerLayoutParams.bottomMargin);
+                        headerLayoutParams.bottomMargin = 0;
+                        header.setLayoutParams(headerLayoutParams);
+                        mState = STATUS_REFRESHING;
+                        updateHeaderView();
+                        if (listener != null) {
+                            mIsLoading = true;
+                            listener.onRefresh();
+                        }
+                        PictureAirLog.e("Ontouc","STATUS_RELEASE_TO_REFRESH");
                     } else if (mState == STATUS_PULL_TO_REFRESH) {
                         // 松手时如果是下拉状态，就去调用隐藏下拉头的任务
-                        new HideHeaderTask().execute();
+                        resetView();
+                        PictureAirLog.e("ontouchevent","STATUS_PULL_TO_REFRESH");
                     }
-                    listView.setIsParentMoving(false);
+                    PictureAirLog.e("ontouchevent","out");
                     break;
             }
             // 时刻记得更新下拉头中的信息
@@ -184,106 +223,15 @@ public class ReFreshLayout extends RelativeLayout {
         }
         return false;
     }
-    /**
-     * 下拉头部回滚的速度
-     */
-    public static final int SCROLL_SPEED = -20;
 
-    /**
-     * 正在刷新的任务，在此任务中会去回调注册进来的下拉刷新监听器。
-     *
-     * @author guolin
-     */
-    class RefreshingTask extends AsyncTask<Void, Integer, Void> {
-        boolean moved = false;
-        private void sleep(int time) {
-            try {
-                Thread.sleep(time);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    private void resetView(){
+        headerLayoutParams.bottomMargin = hideHeaderHeight;
+        header.setLayoutParams(headerLayoutParams);
+        if (listView != null) {
+            listView.listScrollTo(0, 0);
         }
-        @Override
-        protected Void doInBackground(Void... params) {
-            int bottomMargin = headerLayoutParams.bottomMargin;
-            int transY = 0;
-            while (true) {
-                bottomMargin = bottomMargin + SCROLL_SPEED;
-                if (bottomMargin <= 0) {
-                    transY = SCROLL_SPEED - bottomMargin;
-                    bottomMargin = 0;
-                    break;
-                }
-                publishProgress(bottomMargin,SCROLL_SPEED);
-            }
-            mState = STATUS_REFRESHING;
-            publishProgress(0,transY);
-            if (listener != null) {
-                mIsLoading = true;
-                listener.onRefresh();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... bottomMargin) {
-            updateHeaderView();
-            headerLayoutParams.bottomMargin = bottomMargin[0];
-            header.setLayoutParams(headerLayoutParams);
-            if (bottomMargin[0] != 0) {
-                translationY(listView, SCROLL_SPEED);
-            } else {
-                translationY(listView, bottomMargin[1]);
-            }
-        }
-    }
-
-    /**
-     * 隐藏下拉头的任务，当未进行下拉刷新或下拉刷新完成后，此任务将会使下拉头重新隐藏。
-     *
-     * @author guolin
-     */
-    class HideHeaderTask extends AsyncTask<Void, Integer, Integer> {
-        private void sleep(int time) {
-            try {
-                Thread.sleep(time);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        @Override
-        protected Integer doInBackground(Void... params) {
-            int bottomMargin = headerLayoutParams.bottomMargin;
-            while (true) {
-                bottomMargin = bottomMargin + SCROLL_SPEED;
-                if (bottomMargin <= hideHeaderHeight) {
-                    bottomMargin = hideHeaderHeight;
-                    break;
-                }
-                publishProgress(bottomMargin,SCROLL_SPEED);
-                sleep(10);
-            }
-            return bottomMargin;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... bottomMargin) {
-            headerLayoutParams.bottomMargin = bottomMargin[0];
-            header.setLayoutParams(headerLayoutParams);
-            if (bottomMargin[0] != hideHeaderHeight) {
-                translationY(listView,-SCROLL_SPEED);
-            }else{
-                translationY(listView,0);
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Integer bottomMargin) {
-            headerLayoutParams.bottomMargin = bottomMargin;
-            header.setLayoutParams(headerLayoutParams);
-            mState = STATUS_REFRESH_FINISHED;
-            mIsLoading = false;
-        }
+        allMove = 0;
+        isPull = false;
     }
 
     /**
@@ -291,10 +239,10 @@ public class ReFreshLayout extends RelativeLayout {
      */
     public void finishRefreshing() {
         mState = STATUS_REFRESH_FINISHED;
-        translationY(listView,0);
-        headerLayoutParams.bottomMargin = hideHeaderHeight;
-        header.setLayoutParams(headerLayoutParams);
+        resetView();
         mIsLoading = false;
+        allMove = 0;
+        isPull = false;
     }
 
     private void updateHeaderView() {
@@ -369,7 +317,9 @@ public class ReFreshLayout extends RelativeLayout {
         boolean result=false;
         if (listView.getLastVisiblePosition() == (listView.getCount() - 1)) {
             final View bottomChildView = listView.getChildAt(listView.getLastVisiblePosition() - listView.getFirstVisiblePosition());
-            result= (listView.getHeight()>=bottomChildView.getBottom());
+            PictureAirLog.e("listView.getHeight()",String.valueOf(listView.getHeight()));
+            PictureAirLog.e("bottomChildView.getBottom()",String.valueOf(bottomChildView.getBottom()));
+            result= (listView.getHeight()<=bottomChildView.getBottom());
         }
         return  result;
     }
