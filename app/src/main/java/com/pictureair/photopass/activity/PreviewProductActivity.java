d4325fa45dfb9e9b015f9687ae7f098886dd42b1
Bauer_Bao <bauer.bao@pictureworks.biz>
2016-10-14 11:32:38 +0800

    1.无

--- app/src/main/java/com/pictureair/photopass/activity/PreviewProductActivity.java
+++ app/src/main/java/com/pictureair/photopass/activity/PreviewProductActivity.java
@@ -2,7 +2,6 @@ package com.pictureair.photopass.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
